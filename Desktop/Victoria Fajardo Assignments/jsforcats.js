f72af4d1631f64d2055712f0af26f078b08ab5e7
Victoria Fajardo <victoria.fajardo@gmail.com>
2016-03-27 18:04:59 -0500

    game2

---
+++ Desktop/Victoria Fajardo Assignments/jsforcats.js
@@ -0,0 +1,62 @@
+var string = "hello this is cats"
+string
+string + "!!!!!"
+function makeMoreExciting(string) {
+  return string + "!!!!!"
+}
+makeMoreExciting("lounging")
+
+var string = "hello this is cats"
+string + "!!!!!"
+function makeMoreExciting(string){
+return string + "!!!!!"
+{
+makeMoreExciting("lounging")
+
+
+var string = "hello this is cats"
+string + "!!!!!"
+//RUN
+function makeMoreExciting(string) {
+return string + "!!!!!"
+}
+//RUN and then TYPE
+makeMoreExciting("lounging")
+
+var sentence = "time for a nap"
+makeMoreExciting(sentence)
+//this linked sentence/time for a nap to makeMoreExciting/!!!!
+//combined the two
+
+var sentence = "time for a nap"
+sentence = makeMoreExciting(sentence)
+
+function yellIt(string) {
+  string = string.toUpperCase()
+  string = makeMoreExciting(string)
+  console.log(string)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
